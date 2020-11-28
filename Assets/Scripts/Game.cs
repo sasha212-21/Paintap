e29aadfa0dc683354ade6e0e85845661f455f1f8
sasha212-21 <59872181+sasha212-21@users.noreply.github.com>
2020-11-28 15:57:02 +0200

    Add files via upload

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,29 +3,41 @@ using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
+    [Header("Валюты")]
     private int score;
     public Text scoreText;
 
+    //private int gems;
+    //public Text gemsText;
+    //private int coins;
+    //public Text coinsText;
+
     [Header("Магазин улучшений")]
-    public GameObject shopPan;
+    //public GameObject shopPan;
     private int bonus;
     public int[] shopCost;
     public int[] shopBonus;
     public Text[] shopButtText;
 
-    [Header("Магазин роботников")]
-    public GameObject shopPan;
+    //private int[] buyProgress;
+    //public int[] ultimateGoalProgress;
+    //public Text[] buyProgressText;
+    //public Text[] ultimateGoalProgressText;
+
+    //[Header("Магазин роботников")]
+    //public GameObject shopPanWorker;
 
 
     private void Update()
     {
-        scoreText.text = score + "$";
+        scoreText.text = score;
     }
 
     public void OnClick()
     {
         score += bonus + 1;
     }
+
 
     public void shopButt_AddBonus(int index)
     {
@@ -34,23 +46,22 @@ public class Game : MonoBehaviour
             bonus += shopBonus[index];
             score -= shopCost[index];
             shopCost[index] *= 2;
-            shopButtText[index].text = shopCost[index] + "$";
+            shopButtText[index].text = shopCost[index];
         }
         else
         {
             {
-                Debug.Log("Вам не хватает денег!" + " У вас " + score + ", а улучшение стоит " + shopCost[index]);
+                Debug.Log("Вам не хватает ляпов!" + " У вас " + score + ", а улучшение стоит " + shopCost[index]);
             }
         }
     }
 
 
-
-    public void shopPan_ShowAndHide()
+    //Код для открытия и зак. панели магазина
+    /*public void shopPan_ShowAndHide()
     {
         shopPan.SetActive(!shopPan.activeSelf);
-    }
-
+    }*/
 
 
 }
