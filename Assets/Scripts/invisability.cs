db2ec1ce99dd4522ffec7ecff1623015c3ac8042
DivineM0on <58951904+DivineM0on@users.noreply.github.com>
2020-11-30 18:56:37 +0200

    Add files via upload

---
+++ Assets/Scripts/invisability.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invisability : MonoBehaviour
+{
+    public GameObject shopPan;
+
+    public void shopPan_HideandShow()
+    {
+        shopPan.SetActive(!shopPan.activeSelf);
+    }
+}
