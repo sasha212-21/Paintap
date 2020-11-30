3367aca0775b524c983d18e48e7b24fb9a637028
ivanhaidei <59264568+ivanhaidei@users.noreply.github.com>
2020-11-30 19:12:03 +0200

    Add files via upload

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,117 +1,54 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+
+
 
 public class Game : MonoBehaviour
 {
-    
-    [Header("Валюты------------------------------------------")]
     public Text scoreText;
-    public Text scoreEarningText;
-    private double score;
-    
-    /*private int gems;
-    public Text gemsText;
-    private int coins;
-    public Text coinsText;*/
+    public int score;
+    private int bonus = 1;
+    [Header("Shop")]
+    public int[] shopCosts;
+    public int[] shopBonuses;
+    public Text[] shopBttnsText;
+    public GameObject shopPan;
+    public GameObject Settings;
 
-    [Header("Платные бонусы----------------------------------")]
-    public Button[] boostButt;
-    public float[] boostTime;
-    public double boostBonus = 1;
-    public double[] boostPrice;
 
-    [Header("Магазин улучшений-------------------------------")]
-    public double autoBonus;
-    private bool firstBuying;
-    private double clickBonus;
-    public int[] shopPrice;
-    public double[] shopBonus;
-    public Text[] shopButtText;
-    public int[] buyProgress;
-    public int[] ultimateGoalProgress;
-    public Text[] buyProgressText;
-
-    private void Start()
+    private void  Update ()
     {
-        StartCoroutine(bonusPerSecond());
+        scoreText.text = score + "$";
     }
 
-    private void Update()
+    public void shopPan_ShowAndHIde ()
     {
-        scoreText.text = score.ToString();
-        scoreEarningText.text = (autoBonus * 2 * boostBonus).ToString() + "  per/sec";
+        shopPan.SetActive(!shopPan.activeSelf);
     }
-
+    public void Settings_ShowAndHIde()
+    {
+        Settings .SetActive(!Settings.activeSelf);
+    }
+    public void shopBttn_addBonus(int index)
+    {
+        if  (score >= shopCosts[index])
+        {
+            bonus += shopBonuses[index];
+            score -= shopBonuses[index];
+            shopCosts[index] *= 2;
+            shopBttnsText[index].text = "Buy upgrade" + shopCosts[index] + "$";
+        }
+        else
+        {
+            Debug.Log("No money");
+        }
+     
+    }
+   
     public void OnClick()
     {
-        score += clickBonus + 1;
-    }
-
-    public void shopButt_AddBonus(int index)
-    {
-        if (score >= shopPrice[index])
-        {
-            if (buyProgress[index] < ultimateGoalProgress[index])
-            {
-                firstBuying = true;
-                //bonus += shopBonus[index]; бонус до кликов за покупку цвета
-                score -= shopPrice[index];
-
-                if (buyProgress[index] != ultimateGoalProgress[index] - 1)
-                {
-                    shopPrice[index] *= 2;
-                    shopButtText[index].text = shopPrice[index].ToString();
-                }
-                else
-                    shopButtText[index].text = "Soldout!";
-
-                buyProgress[index]++;
-                buyProgressText[index].text = buyProgress[index].ToString();
-                autoBonus += shopBonus[index];
-            }
-            else
-            {
-                Debug.Log("Этот цвет больше не доступен!");
-            }
-        }
-        else
-        {
-            Debug.Log("Вам не хватает ляпов!" + " У вас " + score + ", а улучшение стоит " + shopPrice[index]);
-        }
-    }
-
-    public void OnClickBoost(int index)
-    {
-        if (firstBuying)
-        {
-            if (boostPrice[index] <= score)
-            {
-                score -= boostPrice[index];
-                StartCoroutine(boostAutoBonus(boostTime[index], index));
-            }
-            else
-                Debug.Log("У вас не хватает валюты(");
-        }
-        else
-            Debug.Log("Вы еще не купили ни один цвет, улучшение бесполезно(");
-    }
-
-    IEnumerator bonusPerSecond()
-    {
-        while (true)
-        {
-            score += autoBonus * boostBonus;
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
-
-    IEnumerator boostAutoBonus(float time, int index)
-    {
-        boostButt[index].interactable = false;
-        boostBonus *= 2;
-        yield return new WaitForSeconds(time);
-        boostBonus /= 2;
-        boostButt[index].interactable = true;
+        score += bonus;
+        
     }
 }
