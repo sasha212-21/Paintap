84c50bbadf78fed806c182861c9cab678d0e99ee
DivineM0on <58951904+DivineM0on@users.noreply.github.com>
2020-12-02 13:44:57 +0200

    Add files via upload

--- Assets/Scripts/invisability.cs
+++ Assets/Scripts/invisability.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class invisability : MonoBehaviour
 {
     public GameObject shopPan;
+    public GameObject setings;
+    public GameObject Palettes;
 
     public void shopPan_HideandShow()
     {
         shopPan.SetActive(!shopPan.activeSelf);
+    }
+
+    public void setings_HideandShow()
+    {
+        setings.SetActive(!setings.activeSelf);
+    }
+
+    public void Palettes_HideandShow()
+    {
+        Palettes.SetActive(!Palettes.activeSelf);
     }
 }
